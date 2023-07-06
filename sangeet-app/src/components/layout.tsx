6801b18fdcc5b9623fcbb095c8fde13a984a9330
Binish Joshi <binishjoshi@proton.me>
2023-07-06 11:47:24 +0545

    dark mode

--- sangeet-app/src/components/layout.tsx
+++ sangeet-app/src/components/layout.tsx
@@ -1,11 +1,19 @@
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import TopBar from './top-bar';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 export default function Layout({ children }) {
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <TopBar />
       <main>{children}</main>
       <div>footer</div>
-    </>
+    </ThemeProvider>
   );
 }
